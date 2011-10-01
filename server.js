c870530bc7602f5955725cd4990e62b0d0eab7d2
Rowan Crawford <wombleton@gmail.com>
2011-10-01 13:47:37 +1300

    no cluster on no.de

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ cs = require('coffee-script')
 cluster = require('cluster')
 
 if (process.env.NODE_ENV === 'production') {
-  cluster('app').listen(80);
+  require('./app').listen(80);
 } else {
   require('./app').listen(3000);
 }
